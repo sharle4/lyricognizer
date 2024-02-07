02d3e68a557d2d1bcb9d004c3cacdaf5ab6a1a90
sharle4 <charleskayssieh@gmail.com>
2024-02-07 19:44:08 +0100

    épuration du texte + création lyrics grabber

--- dataset_processor.py
+++ dataset_processor.py
@@ -1,10 +1,11 @@
 from cleaner import preprocess_text,save_preprocessed_text
 import os
 
-songs = os.listdir("./genius-lyrics-api-master/lib/dataset/freeze_corleone")
+songs = os.listdir("./lyrics/bbj/raw")
 
 if __name__ == "__main__":
     for file in songs:
-        file_path = "./genius-lyrics-api-master/lib/dataset/freeze_corleone/" + file
-        save_preprocessed_text(preprocess_text(file_path),file_path)
-
+        file_path = "./lyrics/bbj/processed/" + file
+        preprocessed_text = preprocess_text(file_path)
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(preprocessed_text)
