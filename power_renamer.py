76c82e9a4a6e1228f8351f45d20e72e5cdcbc7d5
sharle4 <charleskayssieh@gmail.com>
2024-02-08 09:00:55 +0100

    power renamer v2

--- power_renamer.py
+++ power_renamer.py
@@ -1,21 +1,29 @@
 import os
 import random
 
-# Chemin du dossier contenant les fichiers à renommer
-dossier = "./lyrics/nekfeu/raw/"
+import os
 
-# Liste des noms de fichiers dans le dossier
-noms_fichiers = os.listdir(dossier)
+# Chemin du dossier principal contenant les dossiers d'artistes
+dossier_principal = "./lyrics/"
 
-# Mélanger aléatoirement la liste des noms de fichiers
-random.shuffle(noms_fichiers)
+# Liste des noms d'artistes basée sur les noms de dossiers
+artistes = [dossier for dossier in os.listdir(dossier_principal) if os.path.isdir(os.path.join(dossier_principal, dossier))]
 
-# Renommer les fichiers dans l'ordre aléatoire
-for i, nom_fichier in enumerate(noms_fichiers, start=1):
-    chemin_origine = os.path.join(dossier, nom_fichier)
-    extension = os.path.splitext(nom_fichier)[1]
-    nouveau_nom = f"{i}{extension}"
-    chemin_destination = os.path.join(dossier, nouveau_nom)
-    os.rename(chemin_origine, chemin_destination)
+# Parcours des dossiers pour chaque artiste
+for artiste in artistes:
+    dossier = f"{dossier_principal}{artiste}/raw"
+    print(dossier)
+    # Liste des noms de fichiers dans le dossier
+    noms_fichiers = os.listdir(dossier)
 
-print("Les fichiers ont été renommés dans un ordre aléatoire.")
+    # Mélanger aléatoirement la liste des noms de fichiers
+    random.shuffle(noms_fichiers)
+
+    # Renommer les fichiers dans l'ordre aléatoire
+    for i, nom_fichier in enumerate(noms_fichiers, start=1):
+        chemin_origine =f"{dossier}/{nom_fichier}"
+        nouveau_nom = f"_{i}.txt"
+        chemin_destination = f"{dossier}/{nouveau_nom}"
+        os.rename(chemin_origine, chemin_destination)
+
+    print("Les fichiers ont été renommés dans un ordre aléatoire.")
