aa4f207c7c674e0686b189e22604a507c267a5f3
sharle4 <charleskayssieh@gmail.com>
2024-02-08 22:58:37 +0100

    finalisation main pour test complet

--- main.py
+++ main.py
@@ -6,15 +6,11 @@ from ngd import *
 from plotter import bar_chart
 
 def mean(L):
-    """donne la moyenne d'une liste de flottants""" 
-    #return sum(L)/len(L)
+    """donne la moyenne d'une liste d'entiers""" 
     if L ==[]:
         return 0
     else:
-        sum = 0
-        for val in L:
-            sum+=val
-        return sum/len(L)
+        return(sum(L)/len(L))
 
 def get_ngd(text,artist):
     """donne la moyenne des ngd entre les mots les plus fréquents du texte et le nom de l'artiste """
@@ -35,42 +31,44 @@ if __name__ == "__main__":
     artists = []
     for folder in os.listdir("./lyrics"):
         artists.append(folder)
-        
-    dico, dico_zlib, dico_lzma, dico_bz2, dico_ngd = {}, {}, {}, {}, {}
-    text_to_test = str(input("Quelles sont vos paroles?"))
-
-    for artist in artists:
-        #dico_ngd[artist] = 1-get_ngd(text_to_test,artist)
-        
-        distances_zlib = []
-        distances_lzma = []
-        distances_bz2 = []
-        path = f"./lyrics/{artist}/processed"
-        songs = os.listdir(path)
-        
-        for song in songs:
-            with open(f"{path}/{song}", 'r', encoding='utf-8') as file:
-                referring_text = file.read()
-            distance_zlib = ncd(text_to_test, referring_text, 'zlib')
-            distance_lzma = ncd(text_to_test, referring_text, 'lzma')
-            distance_bz2 = ncd(text_to_test, referring_text, 'bz2')
-            distances_zlib.append(1-distance_zlib)
-            distances_lzma.append(1-distance_lzma)
-            distances_bz2.append(1-distance_bz2)
-        dico_lzma[artist] = mean(distances_lzma)
-        dico_zlib[artist] = mean(distances_zlib)
-        dico_bz2[artist] = mean(distances_bz2)
-        dico[artist] = (dico_lzma[artist]+dico_zlib[artist])/3             #+dico_ngd[artist])/3
-    print(f"Dico_zlib: {dico_zlib}")
-    print(f"dico_lzma: {dico_lzma}")
-    print(f"dico: {dico}")
-    #print(f"dico_ngd: {dico_ngd}")
-    most_likely_artist = max(dico, key=dico.get)            #variable de qui donne l'artiste le plus proche
-    print("l'artiste qui a le plus de chance d'écrire ces paroles est:",most_likely_artist)
-    bar_chart(dico_bz2,dico_lzma,dico_zlib,dico)
+    results = []
+    for text_format in ["processed", "processed_sw", "processed_lm"]:
+        for test_artist in artists:
+            res_test_artist = []
+            rank = [0, 0, 0]
+            count_base_music = 0
+            for test_song in os.listdir(f"./lyrics/{test_artist}/{text_format}"):
+                res_test_song = {"zlib" : [{}], "lzma" : [{}], "bz2" : [{}]}
+                with open(f"./lyrics/{test_artist}/{text_format}/{test_song}", 'r', encoding='utf-8') as file:
+                    text_test_song = file.read()
+                print(f"Méthode de formattage : {text_format} - Analyse des paroles de la chanson {test_song} de {test_artist}")
+                
+                for base_artist in artists:
+                    distances_zlib = []
+                    distances_lzma = []
+                    distances_bz2 = []
+                    for base_song in os.listdir(f"./lyrics/{base_artist}/{text_format}"):
+                        with open(f"./lyrics/{base_artist}/{text_format}/{base_song}", 'r', encoding='utf-8') as file:
+                            text_base_song = file.read()
+                        distances_zlib.append(1-ncd(text_test_song, text_base_song, 'zlib'))
+                        distances_lzma.append(1-ncd(text_test_song, text_base_song, 'lzma'))
+                        distances_bz2.append(1-ncd(text_test_song, text_base_song, 'bz2'))
+                        count_base_music += 1
+                    res_test_song["zlib"][0][f"{base_artist}"[2:]] = mean(distances_zlib)
+                    res_test_song["lzma"][0][f"{base_artist}"[2:]] = mean(distances_lzma)
+                    res_test_song["bz2"][0][f"{base_artist}"[2:]] = mean(distances_bz2)
+                    rank_artist_zlib = sorted(res_test_song["zlib"][0].values(), reverse=True).index(res_test_song["zlib"][0][f"{base_artist}"[2:]]) + 1
+                    rank_artist_lzma = sorted(res_test_song["lzma"][0].values(), reverse=True).index(res_test_song["lzma"][0][f"{base_artist}"[2:]]) + 1
+                    rank_artist_bz2 = sorted(res_test_song["bz2"][0].values(), reverse=True).index(res_test_song["bz2"][0][f"{base_artist}"[2:]]) + 1
+                    res_test_song["zlib"].append(rank_artist_zlib)
+                    res_test_song["lzma"].append(rank_artist_lzma)
+                    res_test_song["bz2"].append(rank_artist_bz2)
+                    rank[0] += rank_artist_zlib
+                    rank[1] += rank_artist_lzma
+                    rank[2] += rank_artist_bz2
+                res_test_artist.append(res_test_song)
+            rank = rank/count_base_music
+            res_test_artist.append({"zlib" : rank[0], "lzma" : rank[0], "bz2" : rank[0]})
     
-        
-
-
-
-
+    final_results = {artists[i][2:]: results[i] for i in range(len(artists))}
+    print(final_results)
